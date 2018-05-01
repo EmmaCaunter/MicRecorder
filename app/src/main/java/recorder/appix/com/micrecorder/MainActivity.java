328a541fe5433d130a785285af71ac4124b252cc
Emma Caunter <emmacaunter@gmail.com>
2018-04-30 18:51:17 -0700

    added sample rate options

--- app/src/main/java/recorder/appix/com/micrecorder/MainActivity.java
+++ app/src/main/java/recorder/appix/com/micrecorder/MainActivity.java
@@ -1,17 +1,23 @@
 package recorder.appix.com.micrecorder;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
@@ -20,6 +26,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -32,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
     private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
-    private static int SAMPLE_RATE = 44100;
+    private static Integer[] SAMPLE_RATES = { 44100, 44800, 88200, 96000 };
     private static String LOG_TAG = "Mic Recorder";
+
+    private int currentSampleRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +53,49 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(v -> startRecording());
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
+        Integer[] availableSampleRates = getAvailableSampleRates();
         findViewById(R.id.stop_button).setOnClickListener(v -> stopRecording());
+        Spinner sampleSpinner = findViewById(R.id.sampling_rate);
+        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, availableSampleRates);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        sampleSpinner.setAdapter(dataAdapter);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int index = preferences.getInt("SampleRate", 0);
+        currentSampleRate = SAMPLE_RATES[index];
+        sampleSpinner.setSelection(index);
+        sampleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                currentSampleRate = SAMPLE_RATES[i];
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("SampleRate", i).commit();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private Integer[] getAvailableSampleRates() {
+        ArrayList<Integer> available = new ArrayList();
+        for (int i: SAMPLE_RATES) {
+            if (isSampleRateValid(i)) {
+                available.add(i);
+            }
+        }
+        Integer[] finalArray = new Integer[available.size()];
+        int count = 0;
+        for (int j: available) {
+            finalArray[count] = j;
+            count += 1;
+        }
+        return finalArray;
     }
 
     @Override
@@ -69,6 +120,15 @@ public class MainActivity extends AppCompatActivity {
         return file;
     }
 
+    public boolean isSampleRateValid(int rate) {
+        int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        if (bufferSize > 0) {
+            Log.e("Buffer", "Buffer size: " + bufferSize);
+            return true;
+        }
+        return false;
+    }
+
     private void startRecording() {
         if (!isRecording) {
             recordAudio();
@@ -87,18 +147,18 @@ public class MainActivity extends AppCompatActivity {
                     android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
 
                     // buffer size in bytes
-                    int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+                    int bufferSize = AudioRecord.getMinBufferSize(currentSampleRate,
                             AudioFormat.CHANNEL_IN_MONO,
                             AudioFormat.ENCODING_PCM_16BIT);
 
                     if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
-                        bufferSize = SAMPLE_RATE * 2;
+                        bufferSize = currentSampleRate * 2;
                     }
 
                     short[] audioBuffer = new short[bufferSize / 2];
 
                     AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
-                            SAMPLE_RATE,
+                            currentSampleRate,
                             AudioFormat.CHANNEL_IN_MONO,
                             AudioFormat.ENCODING_PCM_16BIT,
                             bufferSize);
